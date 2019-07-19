cea9785bb0f02da971555e786848e15970146ca8
Kristian Røste <github@roste.org>
2019-07-19 10:11:10 +0200

    Update serverquery.php

--- queries/serverquery.php
+++ queries/serverquery.php
@@ -1,1 +1,1 @@
-<?php # include("serverquery/index.php"); ?>
+<?php include("serverquery/index.php") or die("ServerQuery not installed. See <a target='_blank' href='https://github.com/xPaw/PHP-Source-Query'>https://github.com/xPaw/PHP-Source-Query</a>"); ?>
