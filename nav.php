71d472e815ba0bae92bbc35d9dcceb5ee1f1cbfb
Kristian Røste <github@roste.org>
2019-07-19 10:10:59 +0200

    added icons to nav

--- nav.php
+++ nav.php
@@ -1,22 +1,22 @@
 <br>
 <ul class="nav nav-pills nav-fill">
   <li class="nav-item">
-    <a class="nav-link" href="?page=characters">Characters</a>
+    <a class="nav-link" href="?page=characters"><?php echo icon("person"); ?> Characters</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="?page=players">Players</a>
+    <a class="nav-link" href="?page=players"><?php echo icon("player"); ?> Players</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="?page=alliances">Alliances</a>
+    <a class="nav-link" href="?page=alliances"><?php echo icon("alliance"); ?> Alliances</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="?page=bans"><?php echo icon("ban-8x"); ?> Bans</a>
+    <a class="nav-link" href="?page=bans"><?php echo icon("bans"); ?> Bans</a>
   </li>
   <li class="nav-item">
     <a class="nav-link" href="?page=logs"><?php echo icon("align-left-8x"); ?> Logs</a>
   </li>
-  <!--<li class="nav-item">
-    <a class="nav-link" href="?page=serverquery">ServerQuery</a>
-  </li>-->
+  <li class="nav-item">
+    <a class="nav-link" href="?page=serverquery"><?php echo icon("steam"); ?> ServerQuery</a>
+  </li>
 </ul>
 <br>
