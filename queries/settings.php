0617c395dd9f32305b98bb506a9b5dcadd47208a
Kristian Røste <Kristian.Roste@miljodir.no>
2019-08-13 06:19:24 +0200

    title and settings updated

--- queries/settings.php
+++ queries/settings.php
@@ -1,10 +1,19 @@
 <?php
 $jsonfile = "settings.json";
+$jsonfile = file_get_contents($jsonfile);
 $json = json_decode($jsonfile, true);
 
+
+echo "
+<form action='' method='POST'>
+";
 $i = 0; # index
 foreach ($json as $setting=>$value) {
     echo $setting." <input type='text' name='$setting' value='".$value."'";
     $i++;
 }
+echo "
+<input type='submit' value='Save'>
+</form>
+";
 ?>
